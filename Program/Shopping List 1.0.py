230b8426780db4844e755e4dde83ad80b39a1f38
kylevincent <kyle.vincent@my.jcu.edu.au>
2016-09-06 22:44:41 +1000

    Added the options M, and R to the menu, currently working on them

--- Program/Shopping List 1.0.py
+++ Program/Shopping List 1.0.py
@@ -1,5 +1,6 @@
 import csv
 def main():
+    load_information()
     while True:
         mainMenuOutput = mainMenu()
         items = mainMenuOutput[1]
@@ -10,16 +11,18 @@ def main():
         if mainMenuOutput[0] == 'A':
             required_items()
         if mainMenuOutput[0] == 'M':
-            required_items()
+            mark_completed()
         if mainMenuOutput[0] == 'Q':
             break
 
+def load_information():
+    items = open("items.csv")
+    numberOfItems = sum(1 for row  in items)  # calculates the number of items
+    print("shopping list 1.0 - by Kyle Vincent")
+    print("{} items loaded from {}".format(numberOfItems, items.name))
 
 def mainMenu():
     items = open("items.csv")
-    numberOfItems = sum(1 for row in items)  # calculates the number of items
-    print("shopping list 1.0 - by Kyle Vincent")
-    print("{} items loaded from {}".format(numberOfItems, items.name))
     print("Menu:\nR - List required items\nC - List completed items\nA - Add new item\nM - Mark an item as completed\nQ - Quit")
     mainMenuLetters = ["R", "C", "A", "M", "Q"]
     mainMenuInput = input()
@@ -36,14 +39,44 @@ def required_items():
         file = open("items.csv",)
         delimit_file = csv.reader(file, delimiter=",")
         item_storage = []
+        item_number = int(1)
         for line in delimit_file:
             item_storage.append(line)
-        file.close()
-        list_length = len(item_storage)
-        for x in range(0, list_length): #Making progress towards finishing the file calling for loop.
-            print ("x")
+        for row in item_storage: #Making progress towards finishing the file calling for loop.
+            if "r" in row:
+                print("({}) {} ${:>10} ".format(item_number, row[0], row[1], row[2]))
+                item_number += 1
 
-        print(item_storage[0][0])
+        file.close()
         return item_storage
 
+def mark_completed():
+    file = open("items.csv", )
+    delimit_file = csv.reader(file, delimiter=",")
+    item_storage = []
+    item_number = int(0)
+    for line in delimit_file:
+        item_storage.append(line)
+    for row in item_storage:  # calls the file, and prints it with file formating
+        if "r" in row:
+            print("({}) {} ${:>10} ".format(item_number, row[0], row[1], row[2]))
+            item_number += 1
+    interval_conversion = []
+    for row in item_storage:
+        interval_conversion.append(row[1])
+    interval_conversion = [float(i) for i in interval_conversion]
+    expected_price = sum(interval_conversion) #Calculates the expected price and presents it to the user
+    print ("Total expected price for {} items: ${}\nEnter the number of an item to mark as completed".format(item_number, expected_price))
+    #counts number of rows, if input exceeds rows the error, for later, input 1 will be 0 on the list.
+    numberOfItems = sum(1 for row in item_storage)
+    user_input_completed = int(input())
+    while user_input_completed > numberOfItems: #error checking to check variable qualithy,
+        print("Invalid input")
+        user_input_completed = int(input())
+
+
+
+    file.close()
+
 main()
+
